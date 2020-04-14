55f95db1f23cdb1721758d0389e5fc30d7a0d435
Bassel Yasser <basselyasser42@gmail.com>
2020-04-14 21:45:42 +0200

    Update EdgeDetectionFilter.java

--- src/Filters/EdgeDetectionFilter.java
+++ src/Filters/EdgeDetectionFilter.java
@@ -61,8 +61,8 @@ public class EdgeDetectionFilter implements Filter
 	            
 	            		int pixelNeighborhood[][] = 
 	            			{{p1 ,        p2        ,   p3},
-	            	 		{p4 ,        p5        ,   p6},
-	            	 		{p7 ,        p8        ,   p9}};
+	            	 		 {p4 ,        p5        ,   p6},
+	            	 		 {p7 ,        p8        ,   p9}};
 	     
 	
 	            		gxsum = 0;
